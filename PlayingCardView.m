28c07823ec2840be86b97391364d2f8ab94b1d2d
Len Li <leonard.li@gmail.com>
2014-02-20 21:01:54 -0500

    Refactor customizable variables and make font smaller

--- PlayingCardView.m
+++ PlayingCardView.m
@@ -15,22 +15,18 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Customize Card Label Alignment
-        NSInteger cardHeight = self.bounds.size.height;
-        NSInteger cardWidth = self.bounds.size.width;
-        
-        NSInteger xPadding = 3;
-        NSInteger yPadding = 15;
-        
-        NSInteger topOriginXOffset = 5;
-        NSInteger topOriginYOffset = 5;
-        NSInteger bottomOriginYOffset = cardHeight - 25;
-        
-        NSInteger cardLabelWidth = 50;
+        NSInteger cardHeight = cardView.bounds.size.height;
+        NSInteger cardWidth = cardView.bounds.size.width;
+        NSInteger cardLabelWidth = 20;
         NSInteger cardLabelHeight = 20;
-        
+
+        NSInteger xOffset = 0;
+        NSInteger yOffset = 0;
+        NSInteger yPadding = 10;
+
         NSString *cardFontFamily = @"TimesNewRomanPS-BoldMT";
-        CGFloat cardRankFontSize = 16;
-        CGFloat cardSuitFontSize = 20;
+        CGFloat cardRankFontSize = 12;
+        CGFloat cardSuitFontSize = 16;
         
         // Rounded Borders
         self.layer.cornerRadius = 5.0;
@@ -44,34 +40,46 @@
         self.backgroundColor = [UIColor whiteColor];
         
         // Create Label for Top Left Rank
-        UILabel *topRankLabel = [[UILabel alloc] initWithFrame:CGRectMake(topOriginXOffset, topOriginYOffset, cardLabelWidth, cardLabelHeight)];
-        topRankLabel.text = self.rank;
+        UILabel *topRankLabel = [[UILabel alloc] initWithFrame:CGRectMake(xOffset, yOffset, cardLabelWidth, cardLabelHeight)];
+//      topRankLabel.layer.borderWidth = 1;
+//      topRankLabel.layer.borderColor = [UIColor redColor].CGColor;
+        topRankLabel.text = @"10";
+        topRankLabel.textAlignment = NSTextAlignmentCenter;
         [topRankLabel setFont:[UIFont fontWithName:cardFontFamily size:cardRankFontSize]];
         topRankLabel.textColor = [UIColor blackColor];
-        [self addSubview:topRankLabel];
+        [cardView addSubview:topRankLabel];
         
         // Create Label for Top Left Suit
-        UILabel *topSuitLabel = [[UILabel alloc] initWithFrame:CGRectMake(topOriginXOffset+xPadding, topOriginYOffset + yPadding, cardLabelWidth, cardLabelHeight)];
-        topSuitLabel.text = self.suit;
+        UILabel *topSuitLabel = [[UILabel alloc] initWithFrame:CGRectMake(xOffset, yOffset + yPadding, cardLabelWidth, cardLabelHeight)];
+//      topSuitLabel.layer.borderWidth = 1;
+//      topSuitLabel.layer.borderColor = [UIColor redColor].CGColor;
+        topSuitLabel.text = @"♠";
+        topSuitLabel.textAlignment = NSTextAlignmentCenter;
         topSuitLabel.textColor = [UIColor blackColor];
         topSuitLabel.font = [UIFont fontWithName:cardFontFamily size:cardSuitFontSize];
-        [self addSubview:topSuitLabel];
+        [cardView addSubview:topSuitLabel];
         
         // Create Label for Bottom Right Rank
-        UILabel *bottomRankLabel = [[UILabel alloc] initWithFrame:CGRectMake(-topOriginXOffset, bottomOriginYOffset, cardLabelWidth, cardLabelHeight)];
-        bottomRankLabel.text = self.rank;
-        [bottomRankLabel setFont:[UIFont fontWithName:cardFontFamily size:cardRankFontSize]];
+        UILabel *bottomRankLabel = [[UILabel alloc] initWithFrame:CGRectMake(cardWidth-cardLabelWidth-xOffset, cardHeight-yOffset-cardLabelHeight, cardLabelWidth, cardLabelHeight)];
+//      bottomRankLabel.layer.borderWidth = 1;
+//      bottomRankLabel.layer.borderColor = [UIColor redColor].CGColor;
+        bottomRankLabel.text = @"10";
+        bottomRankLabel.textAlignment = NSTextAlignmentCenter;
         bottomRankLabel.textColor = [UIColor blackColor];
+        [bottomRankLabel setFont:[UIFont fontWithName:cardFontFamily size:cardRankFontSize]];
         [bottomRankLabel setTransform:CGAffineTransformMakeRotation(M_PI)];
-        [self addSubview:bottomRankLabel];
+        [cardView addSubview:bottomRankLabel];
         
         // Create Label for Bottom Right Suit
-        UILabel *bottomSuitLabel = [[UILabel alloc] initWithFrame:CGRectMake(-topOriginXOffset-xPadding, bottomOriginYOffset -yPadding, cardLabelWidth, cardLabelHeight)];
-        bottomSuitLabel.text = self.suit;
-        [bottomSuitLabel setFont:[UIFont fontWithName:cardFontFamily size:cardSuitFontSize]];
+        UILabel *bottomSuitLabel = [[UILabel alloc] initWithFrame:CGRectMake(cardWidth-cardLabelWidth-xOffset, cardHeight-yOffset-yPadding-cardLabelHeight, cardLabelWidth, cardLabelHeight)];
+//      bottomSuitLabel.layer.borderWidth = 1;
+//      bottomSuitLabel.layer.borderColor = [UIColor redColor].CGColor;
+        bottomSuitLabel.text = @"♠";
+        bottomSuitLabel.textAlignment = NSTextAlignmentCenter;
         bottomSuitLabel.textColor = [UIColor blackColor];
+        [bottomSuitLabel setFont:[UIFont fontWithName:cardFontFamily size:cardSuitFontSize]];
         [bottomSuitLabel setTransform:CGAffineTransformMakeRotation(M_PI)];
-        [self addSubview:bottomSuitLabel];
+        [cardView addSubview:bottomSuitLabel];
     }
     return self;
 }
